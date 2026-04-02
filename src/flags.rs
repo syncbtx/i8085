442495466b3bb3165a9800c31f8f6e2af123c6e8
Sync-Decode <synkibe@gmail.com>
2026-04-02 15:49:42 +0300

    added the flags definition

---
+++ src/flags.rs
@@ -0,0 +1,42 @@
+pub struct Flag {
+    pub val: bool,
+}
+
+pub struct Flags {
+    s: Flag,
+    z: Flag,
+    ac: Flag,
+    p: Flag,
+    c: Flag,
+}
+
+impl Flags {
+    pub fn new() -> Self {
+        Flags{
+            s: Flag{val: false},
+            z: Flag { val: false },
+            ac: Flag { val: false },
+            p: Flag { val: false },
+            c: Flag { val: false },
+        }
+    }
+
+    pub fn pack(&self) -> u8 {
+        let mut f = 0;
+        f |= (self.s.val as u8) << 7;
+        f |= (self.z.val as u8) << 6;
+        f |= (self.ac.val as u8) << 4;
+        f |= (self.p.val as u8) << 2;
+        f |= 1 << 1;
+        f |= self.c.val as u8;
+        f
+    }
+
+    pub fn unpack(&mut self, f: u8) {
+        self.s.val = (f & 0x80) != 0;
+        self.z.val = (f & 0x40) != 0;
+        self.ac.val = (f & 0x10) != 0;
+        self.p.val = (f & 0x04) != 0;
+        self.c.val = (f & 0x01) != 0;
+    }
+}
